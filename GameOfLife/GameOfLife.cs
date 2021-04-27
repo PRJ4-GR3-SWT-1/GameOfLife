cbaf5e69de61a80639ae6cb100d7ed63f4387a68
Filip <f-r-c@LAPTOP-BO73C4EV>
2021-04-27 13:21:26 +0200

    back to int[]

--- GameOfLife/GameOfLife.cs
+++ GameOfLife/GameOfLife.cs
@@ -6,22 +6,22 @@ namespace GameOfLife
     {
         private readonly int _gridSize;
         private const int AliveThreshold = 25;
-        private bool[,] _curGrid;
-        private bool[,] _newGrid;
+        private int[,] _curGrid;
+        private int[,] _newGrid;
 
 
         public GameOfLife(int gridSize)
         {
             _gridSize = gridSize;
-            _curGrid = new bool[_gridSize, _gridSize];
-            _newGrid = new bool[_gridSize, _gridSize];
+            _curGrid = new int[_gridSize, _gridSize];
+            _newGrid = new int[_gridSize, _gridSize];
 
             var rnd = new Random();
             for (var row = 0; row < _gridSize; row++)
             {
                 for (var col = 0; col < _gridSize; col++)
                 {
-                    _curGrid[row, col] = (rnd.Next(100) < AliveThreshold) ? true : false;
+                    _curGrid[row, col] = (rnd.Next(100) < AliveThreshold) ? 1 : 0;
                 }
             }
         }
@@ -39,11 +39,11 @@ namespace GameOfLife
                         if (ShallLocationBeAlive(row, col))
                         {
                             ++locationsAlive;
-                            _newGrid[row, col] = true;
+                            _newGrid[row, col] = 1;
                         }
                         else
                         {
-                            _newGrid[row, col] = false;
+                            _newGrid[row, col] = 0;
                         }
                     }
                 }
@@ -57,7 +57,7 @@ namespace GameOfLife
         {
             var liveNeighbors = CalcAliveNeighbors(row, col);
 
-            if (_curGrid[row, col] == true)
+            if (_curGrid[row, col] == 1)
             {
                 if ((liveNeighbors == 2) || (liveNeighbors == 3))
                     return true;    // Alive and with two or three neighbors - live on
@@ -77,52 +77,52 @@ namespace GameOfLife
             if (row == 0)   // Top row
             {
                 if (col == 0)    // Top left-hand corner
-                    liveNeighbors = Convert.ToInt32(_curGrid[row, col + 1]) +
-                                    Convert.ToInt32(_curGrid[row + 1, col]) + Convert.ToInt32(_curGrid[row + 1, col + 1]);
+                    liveNeighbors = _curGrid[row, col + 1] +
+                                       _curGrid[row + 1, col] + _curGrid[row + 1, col + 1];
 
                 else if (col == _gridSize - 1) // Top right-hand corner
-                    liveNeighbors = Convert.ToInt32(_curGrid[row, col - 1]) +
-                                    Convert.ToInt32(_curGrid[row + 1, col - 1]) + Convert.ToInt32(_curGrid[row + 1, col]);
+                    liveNeighbors = _curGrid[row, col - 1] +
+                                       _curGrid[row + 1, col - 1] + _curGrid[row + 1, col];
 
                 else // On top edge
-                    liveNeighbors = Convert.ToInt32(_curGrid[row, col - 1]) + Convert.ToInt32(_curGrid[row, col + 1] )+
-                                    Convert.ToInt32(_curGrid[row + 1, col - 1]) + Convert.ToInt32(_curGrid[row + 1, col]) + Convert.ToInt32(_curGrid[row + 1, col + 1]);
+                    liveNeighbors = _curGrid[row, col - 1] + _curGrid[row, col + 1] +
+                                       _curGrid[row + 1, col - 1] + _curGrid[row + 1, col] + _curGrid[row + 1, col + 1];
             }
             else if (row == _gridSize - 1) // Bottom row
             {
                 if (col == 0) // Bottom left-hand corner
-                    liveNeighbors = Convert.ToInt32(_curGrid[row - 1, col]) + Convert.ToInt32(_curGrid[row - 1, col + 1]) +
-                                    Convert.ToInt32(_curGrid[row, col + 1]);
+                    liveNeighbors = _curGrid[row - 1, col] + _curGrid[row - 1, col + 1] +
+                                                                   _curGrid[row, col + 1];
 
                 else if (col == _gridSize - 1) // Bottom right-hand corner
-                    liveNeighbors = Convert.ToInt32(_curGrid[row - 1, col - 1]) + Convert.ToInt32(_curGrid[row - 1, col]) +
-                                       Convert.ToInt32(_curGrid[row, col - 1]);
+                    liveNeighbors = _curGrid[row - 1, col - 1] + _curGrid[row - 1, col] +
+                                       _curGrid[row, col - 1];
 
                 else // On bottom edge
-                    liveNeighbors = Convert.ToInt32(_curGrid[row - 1, col - 1]) + Convert.ToInt32(_curGrid[row - 1, col]) + Convert.ToInt32(_curGrid[row - 1, col + 1]) +
-                                    Convert.ToInt32(_curGrid[row, col - 1]) + Convert.ToInt32(_curGrid[row, col + 1]);
+                    liveNeighbors = _curGrid[row - 1, col - 1] + _curGrid[row - 1, col] + _curGrid[row - 1, col + 1] +
+                                    _curGrid[row, col - 1] + _curGrid[row, col + 1];
             }
             else if (col == 0)
             {
                 // Must be left edge, since corners are covered above
-                liveNeighbors = Convert.ToInt32(_curGrid[row - 1, col]) + Convert.ToInt32(_curGrid[row - 1, col + 1]) +
-                                                            Convert.ToInt32(_curGrid[row, col + 1]) +
-                                Convert.ToInt32(_curGrid[row + 1, col]) + Convert.ToInt32(_curGrid[row + 1, col + 1]);
+                liveNeighbors = _curGrid[row - 1, col] + _curGrid[row - 1, col + 1] +
+                                                            _curGrid[row, col + 1] +
+                                _curGrid[row + 1, col] + _curGrid[row + 1, col + 1];
             }
             else if (col == _gridSize - 1)
             {
                 // Must be right edge, since corners are covered above
-                liveNeighbors = Convert.ToInt32(_curGrid[row - 1, col - 1]) + Convert.ToInt32(_curGrid[row - 1, col]) +
-                                  Convert.ToInt32(_curGrid[row, col - 1]) +
-                                  Convert.ToInt32(_curGrid[row + 1, col - 1]) + Convert.ToInt32(_curGrid[row + 1, col]);
+                liveNeighbors = _curGrid[row - 1, col - 1] + _curGrid[row - 1, col] +
+                                  _curGrid[row, col - 1] +
+                                  _curGrid[row + 1, col - 1] + _curGrid[row + 1, col];
 
             }
             else
             {
                 // Interior field
-                liveNeighbors = Convert.ToInt32(_curGrid[row - 1, col - 1]) + Convert.ToInt32(_curGrid[row - 1, col]) + Convert.ToInt32(_curGrid[row, col + 1]) +
-                                    Convert.ToInt32(_curGrid[row + 1, col - 1]) + Convert.ToInt32(_curGrid[row + 1, col]) +
-                                    Convert.ToInt32(_curGrid[row + 1, col - 1]) + Convert.ToInt32(_curGrid[row + 1, col]) + Convert.ToInt32(_curGrid[row + 1, col + 1]);
+                liveNeighbors = _curGrid[row - 1, col - 1] + _curGrid[row - 1, col] + _curGrid[row, col + 1] +
+                                    _curGrid[row + 1, col - 1] + _curGrid[row + 1, col] +
+                                    _curGrid[row + 1, col - 1] + _curGrid[row + 1, col] + _curGrid[row + 1, col + 1];
             }
 
 
