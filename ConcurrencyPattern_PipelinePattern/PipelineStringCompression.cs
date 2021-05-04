b3c72e3b662ab3c6cfa69f835a418ec103d1a172
AnnSo <64138186+auAnnSo@users.noreply.github.com>
2021-05-04 13:54:11 +0200

    Pipeline Pattern

---
+++ ConcurrencyPattern_PipelinePattern/PipelineStringCompression.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConcurrencyPattern_PipelinePattern
+{
+    public class PipelineStringCompression
+    {
+        private readonly int _nStrings;
+        private readonly int _stringLength;
+        private readonly string _charsInString;
+
+        double _avgCompressionRatio = 0;
+
+        public PipelineStringCompression(string charsInString, int nStrings, int stringLength)
+        {
+            _charsInString = charsInString;
+            _nStrings = nStrings;
+            _stringLength = stringLength;
+        }
+
+
+        public double Run()
+        {
+            BlockingCollection<string> input = new BlockingCollection<string>();
+            BlockingCollection<string> output = new BlockingCollection<string>();
+            BlockingCollection<string> inputCpy = new BlockingCollection<string>();
+
+
+            Task generateStringsTask = Task.Run(() =>
+            {
+                Stopwatch sw = new Stopwatch();
+                sw.Start();
+                try
+                {
+                    for (var i = 0; i < _nStrings; i++)
+                    {
+                        var str = Generate(_stringLength);
+                        input.Add(str);
+                        inputCpy.Add(str);
+                    }
+
+                }
+                finally
+                {
+                    input.CompleteAdding();
+                    sw.Stop();
+                    Console.WriteLine("Generated strings in time: " + sw.Elapsed);
+                }
+            });
+
+            Task compressStringsTask = Task.Run(() =>
+            {
+                Stopwatch sw = new Stopwatch();
+                sw.Start();
+                try
+                {
+
+                    foreach (var basicstring in input.GetConsumingEnumerable())
+                    {
+                        var basic = Compress(basicstring);
+                        output.Add(basic);
+                    }
+
+                }
+                finally
+                {
+                    output.CompleteAdding();
+                    sw.Stop();
+                    Console.WriteLine("Compressed strings in time: " + sw.Elapsed);
+                }
+            });
+
+            Task updateCompressionStatsTask = Task.Run(() =>
+            {
+                Stopwatch sw = new Stopwatch();
+                sw.Start();
+                var count = 0;
+                try
+                {
+
+                    foreach (var compressedString in output.GetConsumingEnumerable())
+                    {
+                        UpdateCompressionStats(count, inputCpy.ElementAt(count), compressedString);
+                        count++;
+                    }
+                }
+                finally
+                {
+                    Console.WriteLine("Finished: " + count);
+                    sw.Stop();
+                    Console.WriteLine("Updated strings compression ratio in time: " + sw.Elapsed);
+                }
+
+            });
+
+            Task.WaitAll(generateStringsTask, compressStringsTask, updateCompressionStatsTask);
+            return _avgCompressionRatio;
+        }
+
+
+        private void UpdateCompressionStats(int i, string str, string compressedStr)
+        {
+            _avgCompressionRatio = ((i * _avgCompressionRatio) + ((double)(compressedStr.Length) / str.Length)) / (i + 1);
+
+        }
+
+
+        private string Compress(string str)
+        {
+            var result = "";
+            for (var i = 0; i < str.Length; i++)
+            {
+                var j = i;
+                result += str[i];
+                while ((j < str.Length) && (str[i] == str[j]))
+                    j++;
+
+                if (j > i + 1)
+                {
+                    result += (j - i);
+                    i = j - 1;
+                }
+            }
+            return result;
+        }
+
+        private string Generate(int stringLength)
+        {
+            var random = new Random();
+            var result = new string(Enumerable.Repeat(_charsInString, stringLength).Select(s => s[random.Next(s.Length)]).ToArray());
+            return result;
+        }
+
+
+
+
+    }
+}
---
+++ ConcurrencyPattern_PipelinePattern/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace ConcurrencyPattern_PipelinePattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sequential Compression: ");
+            SequentialStringCompression ssc = new SequentialStringCompression("ABC", 100, 25000);
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            Console.WriteLine("Average compression ratio: "+ ssc.Run());
+            stopwatch.Stop();
+            Console.WriteLine("Time used: "+ stopwatch.Elapsed);
+            //1000 strings = 88% = 45 s
+
+            Console.WriteLine("Pipeline Compression: ");
+            PipelineStringCompression psc = new PipelineStringCompression("ABC", 100, 25000);
+            stopwatch.Restart();
+            Console.WriteLine("Average compression ratio: " + psc.Run());
+            stopwatch.Stop();
+            Console.WriteLine("Time used: " + stopwatch.Elapsed);
+
+
+
+
+        }
+    }
+}
